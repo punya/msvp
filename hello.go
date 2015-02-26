6fdbea73f5013d05947c8228214234dec1e9798f
Punya Biswal <pbiswal@palantir.com>
2015-02-26 01:07:04 -0500

    Tagging and saving

--- hello.go
+++ hello.go
@@ -1,14 +1,69 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"appengine"
+	"appengine/search"
+	"encoding/json"
+	"net/http"
 )
 
+type Incident struct {
+	appengine.GeoPoint
+	Text string
+}
+
+type PostResult struct {
+	DocId string
+}
+
 func init() {
-    http.HandleFunc("/posts", handler)
+	http.HandleFunc("/posts", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Mapping Sexual Violence Project")
+	decoder := json.NewDecoder(r.Body)
+
+	c := appengine.NewContext(r)
+	index, err := search.Open("incidents")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+
+	switch r.Method {
+	case "GET":
+		var incidents []Incident
+		for it := index.List(c, nil); ; {
+			var incident Incident
+			_, err := it.Next(&incident)
+			if err == search.Done {
+				break
+			}
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			incidents = append(incidents, incident)
+		}
+		encoder.Encode(incidents)
+
+	case "POST":
+		var incident Incident
+		err := decoder.Decode(&incident)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		id, err := index.Put(c, "", &incident)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		result := PostResult{DocId: id}
+		encoder.Encode(result)
+	}
 }
--- static/app.js
+++ static/app.js
@@ -1,32 +1,54 @@
 (function() {
+  "use strict";
+
   var map = L.map('map');
   L.tileLayer.provider('OpenStreetMap').addTo(map);
   map.locate({setView: true, maxZoom: 16});
 
-  function Incident(text, latitude, longitude) {
+  function Incident(text, lat, lng) {
     this.text = text;
-    this.latitude = latitude;
-    this.longitude = longitude;
+    this.lat = lat;
+    this.lng = lng;
   }
   Incident.prototype.render = function render() {
-    var marker = L.marker([this.latitude, this.longitude]).addTo(map);
+    var marker = L.marker([this.lat, this.lng]).addTo(map);
     marker.bindPopup(this.text);
   }
+  Incident.prototype.save = function save(callback) {
+    var req = new XMLHttpRequest();
+    req.onload = function saveXhrCallback() {
+      if (req.status == 200) {
+        callback(JSON.parse(req.responseText));
+      } else {
+        console.log(req.responseText);
+      }
+    };
+    req.open('post', 'posts', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(this));
+  }
 
-  var incidents = [];
-  incidents.forEach(function render(inc) { inc.render(); });
+  var req = new XMLHttpRequest();
+  req.onload = function loadXhrCallback() {
+    JSON.parse(req.responseText).forEach(function makeObject(json) {
+      new Incident(json.Text, json.Lat, json.Lng).render();
+    });
+  };
+  req.open('get', 'posts', true);
+  req.send();
 
   map.on('contextmenu', function handleLongPress(e) {
-    var marker = L.marker([e.latlng.lat, e.latlng.lng]);
+    var marker = L.marker(e.latlng);
 
     var container = document.createElement('div');
     container.innerHTML = '<textarea></textarea><br><button>Post</button>';
     var storyTextArea = container.querySelector('textarea');
     container.querySelector('button').addEventListener('click', function handlePostButton() {
-      marker.closePopup();
       var incident = new Incident(storyTextArea.value, e.latlng.lat, e.latlng.lng);
-      incidents.push(incident);
       incident.render();
+      incident.save(function afterSave(e) {
+        marker.closePopup();
+      });
     });
     marker.on('popupclose', function handlePostPopupClose() {
       map.removeLayer(marker);
