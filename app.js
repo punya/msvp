326422a23da856b6e6138cffcb7042103661fab8
Punya Biswal <pbiswal@palantir.com>
2015-03-16 12:15:00 -0400

    Make location opt-in, start from India

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import {Incident} from './incident';
 
 var map = L.map('map');
+map.fitBounds([
+  [6.74678, 68.03215],
+  [35.674520, 97.40238]
+]);
 L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
   attribution: `
     Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a>
@@ -9,12 +13,9 @@ L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
   `,
   subdomains: '1234'
 }).addTo(map);
+L.control.locate().addTo(map);
 var markers = new L.MarkerClusterGroup();
 map.addLayer(markers);
-map.locate({
-  setView: true,
-  maxZoom: 16
-});
 map.on('contextmenu', (e) => {
   $("#location").val(e.latlng.lat + "," + e.latlng.lng);
   $("#reportModal").modal();
